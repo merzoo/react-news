b66531e14ca294f7bbc73fbbf06a67cd0dfa8efd
guojinliang <496168739@qq.com>
2019-05-14 10:54:58 +0800

    add concurrent-mode demo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ConcurrentMode from "./Demos/ConcurrentMode";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ConcurrentMode />
+    </div>
+  );
+}
+
+export default App;
